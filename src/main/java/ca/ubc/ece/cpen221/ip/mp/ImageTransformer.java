e413573e6ad0ae2d0f81e918204fb089db056090
qsenych <146891801+qsenych@users.noreply.github.com>
2024-09-30 21:26:55 -0700

    finsihed task1 and halfway through task 2

--- src/main/java/ca/ubc/ece/cpen221/ip/mp/ImageTransformer.java
+++ src/main/java/ca/ubc/ece/cpen221/ip/mp/ImageTransformer.java
@@ -5,6 +5,8 @@ import ca.ubc.ece.cpen221.ip.core.ImageProcessingException;
 import ca.ubc.ece.cpen221.ip.core.Rectangle;
 
 import java.awt.Color;
+import java.util.Collections;
+import java.util.ArrayList;
 
 /**
  * This datatype (or class) provides operations for transforming an image.
@@ -104,12 +106,66 @@ public class ImageTransformer {
      */
     public Image negative() {
         // TODO: Implement this method
-        return null;
+        Image negativeImage = new Image(this.width, this.height);
+        for (int col = 0; col < this.width; col++) {
+            for (int row = 0; row < this.height; row++) {
+                int originalPixel = image.getRGB(col, row);
+
+                int alpha = (originalPixel >> 24) & 0xFF;
+                int red = (originalPixel >> 16) & 0xFF;
+                int green = (originalPixel >> 8) & 0xFF;
+                int blue = originalPixel & 0xFF;
+
+                int negativePixel = (alpha << 24) | ((255 - red) << 16) | ((255 - green) << 8) | (255 - blue);
+
+                negativeImage.setRGB(col, row, negativePixel);
+            }
+        }
+        return negativeImage;
     }
 
     public Image posterize() {
         // TODO: Implement this method
-        return null;
+        Image posterizedImage = new Image(this.width, this.height);
+        for (int col = 0; col < this.width; col++) {
+            for (int row = 0; row < this.height; row++) {
+                int originalPixel = image.getRGB(col, row);
+
+                int alpha = (originalPixel >> 24) & 0xFF;
+                int red = (originalPixel >> 16) & 0xFF;
+                int green = (originalPixel >> 8) & 0xFF;
+                int blue = originalPixel & 0xFF;
+
+                if(red <= 64) {
+                    red = 32;
+                } else if (red <= 128) {
+                    red = 96;
+                } else {
+                    red = 222;
+                }
+
+                if(green <= 64) {
+                    green = 32;
+                } else if (green <= 128) {
+                    green = 96;
+                } else {
+                    green = 222;
+                }
+
+                if(blue <= 64) {
+                    blue = 32;
+                } else if (blue <= 128) {
+                    blue = 96;
+                } else {
+                    blue = 222;
+                }
+
+                int posterizedPixel = (alpha << 24) | (red << 16) | (green << 8) | blue;
+
+                posterizedImage.setRGB(col, row, posterizedPixel);
+            }
+        }
+        return posterizedImage;
     }
 
 
@@ -117,7 +173,66 @@ public class ImageTransformer {
 
     public Image denoise() {
         // TODO: Implement this method
-        return null;
+        Image cleanImage = new Image(this.width, this.height);
+        for (int col = 0; col < this.width; col++) {
+            for (int row = 0; row < this.height; row++) {
+                int originalPixel = image.getRGB(col, row);
+                //(col-1, row+1), (col, row+1),  (col+1, row+1)
+                //(col-1, row)       pixel       (col+1, row)
+                //(col-1, row-1)  (col, row-1),  (col+1, row-1)
+
+                int redSum = 0;
+                ArrayList<Integer> reds = new ArrayList<Integer>();
+                ArrayList<Integer> greens = new ArrayList<Integer>();
+                ArrayList<Integer> blues = new ArrayList<Integer>();
+
+                for(int i = -1; i <= 1; i++) {
+                    for(int j = 1; j >= -1; j--) {
+                        if(col + i >= 0 && col + i < this.width && row + j >= 0 && row + j < this.height) {
+                            reds.add((image.getRGB(col + i, row + j) >> 16) & 0xFF);
+                            greens.add((image.getRGB(col + i, row + j) >> 8) & 0xFF);
+                            blues.add(image.getRGB(col + i, row + j) & 0xFF);
+                            /*
+                            redSum += (image.getRGB(col + i, row + j) >> 16) & 0xFF;
+                            greenSum += (image.getRGB(col + i, row + j) >> 8) & 0xFF;
+                            blueSum += image.getRGB(col + i, row + j) & 0xFF;
+                            neighbourCount++;
+
+                             */
+                        }
+                    }
+                }
+
+                int alpha = (originalPixel >> 24) & 0xFF;
+                int red = listMedian(reds);
+                int green = listMedian(greens);
+                int blue = listMedian(blues);
+
+                int cleanPixel = (alpha << 24) | (red << 16) | (green << 8) | blue;
+
+                cleanImage.setRGB(col, row, cleanPixel);
+            }
+        }
+        return cleanImage;
+    }
+
+    /**
+     * Calculates and returns the median of a list
+     * @param an unsorted ArrayList of integers
+     * @return the median of the ArrayList as an integer
+     */
+    private int listMedian(ArrayList<Integer> list) {
+        Collections.sort(list);
+
+        int middle;
+
+        if(list.size() % 2 == 1) {
+            middle = (list.get(list.size() / 2) + list.get(list.size() / 2 - 1)) / 2;
+        } else {
+            middle = list.get(list.size() / 2);
+        }
+
+        return middle;
     }
 
     /**
