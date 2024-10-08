3275c7635acc4571c287624b7121c578df77e4d3
Quinn <qj.senych@gmail.com>
2024-10-07 20:57:37 -0700

    fixed task3 up to spec

--- src/main/java/ca/ubc/ece/cpen221/ip/mp/ImageProcessing.java
+++ src/main/java/ca/ubc/ece/cpen221/ip/mp/ImageProcessing.java
@@ -19,17 +19,25 @@ public class ImageProcessing {
      * Black images have a similarity of 1.0 with other uniform coloured images.
      * Black images have a similarity of 0.0 with other images.
      *
-     * @param img1 32 bit ARGB or 24 bit RGB image with the same height AND width as img2
-     * @param img2 32 bit ARGB or 24 bit RGB image with the same height AND width as img2
+     * @param img1 32 bit ARGB or 24 bit RGB image with the same height * width as img2
+     * @param img2 32 bit ARGB or 24 bit RGB image with the same height * width as img2
      * @return a double of the cosine similarity
      */
     public static double cosineSimilarity(Image img1, Image img2) {
-        if (img1.width() != img2.width() || img1.height() != img2.height()) {
+        int width1 = img1.width();
+        int height1 = img1.height();
+        int width2 = img2.width();
+        int height2 = img2.height();
+
+        int size1 = width1 * height1;
+        int size2 = width2 * height2;
+
+        if (size1 != size2) {
             throw new IllegalArgumentException();
         }
 
-        int width = img1.width();
-        int height = img1.height();
+        long[] v1 = new long[size1];
+        long[] v2 = new long[size1];
 
         boolean img1SolidColour = true;
         boolean img2SolidColour = true;
@@ -42,25 +50,31 @@ public class ImageProcessing {
 
         int prevGrayVal1 = img1g.getRGB(0, 0) & 0xFF;
         int prevGrayVal2 = img2g.getRGB(0, 0) & 0xFF;
-        int grayVal1;
-        int grayVal2;
+        int grayVal1 = 0;
+        int grayVal2 = 0;
 
         BigInteger numSum = BigInteger.valueOf(0);
         BigInteger denomSum1 = BigInteger.valueOf(0);
         BigInteger denomSum2 = BigInteger.valueOf(0);
 
-        for (int col = 0; col < width; col++) {
-            for (int row = 0; row < height; row++) {
-                grayVal1 = img1g.getRGB(col, row) & 0xFF;
-                grayVal2 = img2g.getRGB(col, row) & 0xFF;
-
+        int index1 = 0;
+        for (int row = 0; row < height1; row++) {
+            for (int col = 0; col < width1; col++) {
+                v1[index1++] = grayVal1 = img1g.getRGB(col, row) & 0xFF;
                 if(grayVal1 != prevGrayVal1) img1SolidColour = false;
-                if(grayVal2 != prevGrayVal2) img2SolidColour = false;
-
-                numSum = numSum.add(BigInteger.valueOf(grayVal1).multiply(BigInteger.valueOf(grayVal2)));
-                denomSum1 = denomSum1.add(BigInteger.valueOf(grayVal1).multiply(BigInteger.valueOf(grayVal1)));
-                denomSum2 = denomSum2.add(BigInteger.valueOf(grayVal2).multiply(BigInteger.valueOf(grayVal2)));
             }
+        }
+        int index2 = 0;
+        for (int row = 0; row < height2; row++) {
+            for (int col = 0; col < width2; col++) {
+                v2[index2++] = grayVal2 = img2g.getRGB(col, row) & 0xFF;
+                if(grayVal2 != prevGrayVal2) img2SolidColour = false;
+            }
+        }
+        for (int i = 0; i < size1; i++) {
+            numSum = numSum.add(BigInteger.valueOf(v1[i]).multiply(BigInteger.valueOf(v2[i])));
+            denomSum1 = denomSum1.add(BigInteger.valueOf(v1[i]).multiply(BigInteger.valueOf(v1[i])));
+            denomSum2 = denomSum2.add(BigInteger.valueOf(v2[i]).multiply(BigInteger.valueOf(v2[i])));
         }
 
         double cSim = numSum.doubleValue() / ( Math.sqrt(denomSum1.doubleValue()) * Math.sqrt(denomSum2.doubleValue()) );
@@ -72,8 +86,8 @@ public class ImageProcessing {
     /**
      * Compares a List of images for the best match to the original image
      *
-     * @param img A non empty image with the same width and height as every image in matchingCandidates
-     * @param matchingCandidates A non-null not empty List of images with the same width and height as img
+     * @param img A non empty image with the same width * height as every image in matchingCandidates
+     * @param matchingCandidates A non-null not empty List of images with the same width * height as img
      * @return A list including all images in matchingCandidates, sorted using cosine similarity, with best match appearing first
      */
     public static List<Image> bestMatch(Image img, List<Image> matchingCandidates) {
